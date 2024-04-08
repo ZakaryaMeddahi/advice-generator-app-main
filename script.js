c5bca59eddeecc3318c5ce8b13677a4b880b8ea8
ZakaryaMeddahi <Z.meddahi2001@gmail.com>
2024-04-08 16:37:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const button = document.getElementById('dice-button');
+const adviceIdWrapper = document.getElementById('advice-id');
+const adviceWrapper = document.getElementById('advice');
+
+const BASE_URL = 'https://api.adviceslip.com';
+
+const getRandomAdvice = async (url) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${url}`);
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const { id, advice } = data.slip;
+
+    adviceIdWrapper.textContent = id;
+    adviceWrapper.textContent = `"${advice}"`;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// window.onload = async () => await getRandomAdvice('advice');
+
+button.onclick = async () => await getRandomAdvice('advice');
